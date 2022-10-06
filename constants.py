9b6aa28cd29f30987ea307cba2fd6de3f784bfeb
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-10-06 17:42:37 +0000

    added constants file

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 Ioana Marinescu All rights reserved.
+# Created By: Ioana Marinescu
+#
+# Date: Sept. 29
+# contains constants for tax.py program
+
+HST = 0.15
