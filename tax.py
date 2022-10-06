621f7d78aa87bd4f60ab1413b506ae6d4f1e083c
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-10-06 17:39:59 +0000

    fixed a thing that was not good

---
+++ tax.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 Ioana Marinescu All rights reserved.
+# Created By: Ioana Marinescu
+#
+# Date: Oct. 5, 2022
+# a program that calculated the tax
+
+
+import constants
+
+def main():
+	# variables 
+	subtotal = float(input("Enter subtotal ($ CAD): "))
+
+	# calculations
+	tax = subtotal * constants.HST
+	total = subtotal + tax
+
+	# displays total amount
+	print(f"Your total cost is ${total:.2f}")
+
+if __name__ == "__main__":
+	main()
+	
